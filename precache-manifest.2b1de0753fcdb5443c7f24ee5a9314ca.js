26493a1b2da2719abe5014ac8d3015242268abeb
Nathanael Mueller <inathan.m@gmail.com>
2020-08-21 13:16:14 -0700

    Updates

---
+++ precache-manifest.2b1de0753fcdb5443c7f24ee5a9314ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4355d79d968b2262383246073d9251f2",
+    "url": "/voter-participation/index.html"
+  },
+  {
+    "revision": "56d7b0f7c9f26b30dd8d",
+    "url": "/voter-participation/static/css/main.a0e2643c.chunk.css"
+  },
+  {
+    "revision": "89b64f62ea2c6b3f0198",
+    "url": "/voter-participation/static/js/2.872dbbd4.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/voter-participation/static/js/2.872dbbd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d7b0f7c9f26b30dd8d",
+    "url": "/voter-participation/static/js/main.aead8b11.chunk.js"
+  },
+  {
+    "revision": "cd5d577d45e142620cf8",
+    "url": "/voter-participation/static/js/runtime-main.240f4ac2.js"
+  }
+]);
